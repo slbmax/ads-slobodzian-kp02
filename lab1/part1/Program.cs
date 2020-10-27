21e8418fe2eafeb17acde8c733e3f811cb8d45d5
slbmax <slobodzian.maxim@gmail.com>
2020-10-27 20:08:28 +0200

    lab1

---
+++ lab1/part1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Enter number y:");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Enter number z:");
+            double z = double.Parse(Console.ReadLine());
+            double ln = Math.Log(Math.Abs(y-x), Math.E);
+            if (z==-x || x==y || ln==-2)
+            {
+            Console.WriteLine("Error:wrong data");
+            }
+            else
+            {
+                double a = 1+Math.Log10(Math.Abs(x+z))/(1+ln/2);
+                double b = 1/(Math.Pow(a, 4)+1); 
+                Console.WriteLine("a is: {0};", a);
+                Console.WriteLine("b is: {0};", b);
+            }
+        }
+    }
+}
---
+++ lab1/part2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the day:");
+            int d = int.Parse(Console.ReadLine());
+            Console.Write("Enter the month:");
+            int m = int.Parse(Console.ReadLine());
+            if (check_the_correct_date(m, d) == false)
+            {
+                Console.WriteLine("Error: wrong entered date or month");
+            }
+            else
+            {
+                int what_week_is = day_sum(m,d)%7;
+                if(what_week_is == 1)
+                Console.WriteLine("Day:{0}, month:{1}, it`s wednesday", d, m);
+                else if(what_week_is == 2)
+                Console.WriteLine("Day:{0}, month:{1}, it`s thursday", d, m);
+                else if(what_week_is == 3)
+                Console.WriteLine("Day:{0}, month:{1}, it`s friday", d, m);
+                else if(what_week_is == 4)
+                Console.WriteLine("Day:{0}, month:{1}, it`s saturday", d, m);
+                else if(what_week_is == 5)
+                Console.WriteLine("Day:{0}, month:{1}, it`s sunday", d, m);
+                else if(what_week_is == 6)
+                Console.WriteLine("Day:{0}, month:{1}, it`s monday", d, m);
+                else
+                Console.WriteLine("Day:{0}, month:{1}, it`s tuesday", d, m);
+            }
+        }
+
+        static int days_in_month(int month)
+        {
+            
+            if (month < 8)
+            {
+                if (month == 2)
+                return 28;
+                else
+                    {
+                    if (month % 2 == 1)
+                    return 31;
+                    else
+                    return 30;
+                    }
+            }
+            else
+            {
+                if (month % 2 == 1)
+                    return 30;
+                else
+                    return 31;
+            }
+        }
+
+        static int day_sum(int m, int d)
+        {
+            int days = 0;
+            for (int n = 1; n < m; n++)
+            {
+                days = days + days_in_month(n);
+            }
+            return days + d;
+
+        }
+        static bool check_the_correct_date(int m, int d)   //ce ne nada sha
+        {
+            if (m > 0 && m < 13 && d > 0 && d < 32)
+            {
+                if (d <= days_in_month(m))
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+    }
+
+
+}
